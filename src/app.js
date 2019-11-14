9cf03f54a05f860b017075088053d6e10da6c097
bkolomoetz <bkolomoetz@gmail.com>
2019-11-14 20:24:48 +0200

    orders import fix

--- src/app.js
+++ src/app.js
@@ -1,8 +1,9 @@
 // this is an example of improting data from JSON
-import 'orders' from '../data/orders.json';
+import orders from '../data/orders.json';
 
 export default (function () {
     // YOUR CODE GOES HERE
     // next line is for example only
+    console.log(orders[1]);
     document.getElementById("app").innerHTML = "<h1>Hello WG Forge</h1>";
 }());
